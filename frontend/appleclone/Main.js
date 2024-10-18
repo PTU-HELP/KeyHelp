1cbeae2acb79eafdf49b3d230689de68bbbda6d9
Junheeee <jjangjun1001@naver.com>
2024-10-18 15:36:20 +0900

    Delete frontend/appleclone directory

--- frontend/appleclone/Main.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    const container = document.querySelector('.container');
-    const scrollPos = window.scrollY;
-    container.style.transform = `translateY(${scrollPos * 0.5}px)`;
-});
